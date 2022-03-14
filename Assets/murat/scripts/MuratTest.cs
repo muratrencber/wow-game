83a52620d45d46515d43a7f3c06c781d67570a35
muratrencber <muratrencber2001@gmail.com>
2022-03-14 17:29:39 +0300

    Initial commit

---
+++ Assets/murat/scripts/MuratTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuratTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("This is a test log!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
