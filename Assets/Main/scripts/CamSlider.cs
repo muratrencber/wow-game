375376e8f4f5a89fc185601b4c89ce5ba9e59552
muratrencber <muratrencber2001@gmail.com>
2022-03-15 03:48:25 +0300

    Synced with camera and slider.

--- Assets/Main/scripts/CamSlider.cs
+++ Assets/Main/scripts/CamSlider.cs
@@ -19,21 +19,22 @@ public class CamSlider : MonoBehaviour
 
     public static void SetCameras(float leftRatio)
     {
-
+        if(!instance)
+            return;
         instance.cam1.rect = new Rect(0, 0, leftRatio, 1);
         instance.cam2.rect = new Rect(leftRatio, 0, 1 - leftRatio, 1);
         float offset = (leftRatio - 1);
-        CinemachineFramingTransposer cft = instance.cam1Machine.GetCinemachineComponent<CinemachineFramingTransposer>();
+        CinemachineTransposer cft = instance.cam1Machine.GetCinemachineComponent<CinemachineTransposer>();
         offset *= instance.cam1Machine.m_Lens.OrthographicSize * 2;
-        Vector3 offsetVec = cft.m_TrackedObjectOffset;
+        Vector3 offsetVec = cft.m_FollowOffset;
         offsetVec.x = offset;
-        cft.m_TrackedObjectOffset = offsetVec;
+        cft.m_FollowOffset = offsetVec;
 
         float otherOffset = leftRatio;
         otherOffset *= instance.cam2Machine.m_Lens.OrthographicSize * 2;
-        CinemachineFramingTransposer cft2 = instance.cam2Machine.GetCinemachineComponent<CinemachineFramingTransposer>();
-        Vector3 offsetVec2 = cft.m_TrackedObjectOffset;
+        CinemachineTransposer cft2 = instance.cam2Machine.GetCinemachineComponent<CinemachineTransposer>();
+        Vector3 offsetVec2 = cft2.m_FollowOffset;
         offsetVec2.x = otherOffset;
-        cft2.m_TrackedObjectOffset = offsetVec2;
+        cft2.m_FollowOffset = offsetVec2;
     }
 }
