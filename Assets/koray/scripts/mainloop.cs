ad093b6380dd521f413c8014fe868d1f4a7fee5f
korpik1212 <korpik123456@gmail.com>
2022-03-15 03:09:31 +0300

    meraba

--- Assets/koray/scripts/mainloop.cs
+++ Assets/koray/scripts/mainloop.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class mainloop : MonoBehaviour
 {
     public GameObject teacup,teapot;
+    public GameObject tempcup,temppot;
+    public dialogue_system ds;
+    
 
+    public void Start(){
 
-    public void askfortea(){
-        //
+    }
+
+    public void askfortea_confirmation(){
+        //ver verme butonu 
+        //ver derse askfor tea çalıştır
+    }
+    public IEnumerator askfortea(){
+        
+        yield return null;
         //
         //spawn action
+        //tea ve cup 'ı belirli yerlerde spawnla
+        //belirli bir süre geçerse kızma sinyali gödner
+        //en sonda yok edicek bunları geri
+    }
+
+
+    public void askfortea_finished(){
+        Destroy(tempcup);
+        Destroy(temppot);
     }
 }
