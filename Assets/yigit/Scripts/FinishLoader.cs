8675a2528f3b8cb58b224df8ce0155b670b1a120
muratrencber <muratrencber2001@gmail.com>
2022-03-17 20:36:54 +0300

    Various changes.

--- Assets/yigit/Scripts/FinishLoader.cs
+++ Assets/yigit/Scripts/FinishLoader.cs
@@ -27,7 +27,6 @@ public class FinishLoader : MonoBehaviour
     }
     public void LoadNextLevel()
     {
-<<<<<<< HEAD
         if(Failed)
             return;
         if(Rival.ReachedCount <= 0)
@@ -37,8 +36,5 @@ public class FinishLoader : MonoBehaviour
             Dad.OnFailedClass();
             Failed = true;
         }
-=======
-        SceneManager.LoadScene("GoodEnding");
->>>>>>> cfbd62430fcd67891481a8d082eabcbaf2d93ba4
     }
 }
